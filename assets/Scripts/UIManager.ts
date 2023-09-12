20e260e95e5d31a36cb8b49bc90ae84b159c85d7
duytin095 <duytin095@gmail.com>
2023-09-13 00:26:50 +0700

    update high score

--- assets/Scripts/UIManager.ts
+++ assets/Scripts/UIManager.ts
@@ -1,4 +1,4 @@
-import { _decorator, Button, Component, director, find, Input, Label, Node, sys } from 'cc';
+import { _decorator, Button, Component, director, find, Input, Label, log, Node, sys } from 'cc';
 import { GameManager } from './GameManager';
 const { ccclass, property } = _decorator;
 
@@ -21,7 +21,7 @@ export class UIManager extends Component {
 
 
     private highScore: number = 0;
-    private currentScore: number;
+    private currentScore: number = 0;
 
     start(){
         this.currentScore = 0;
@@ -45,29 +45,26 @@ export class UIManager extends Component {
         this.updateScore(this.currentScore + addScore);
     }
 
-    
-    // ResetScore(){
-    //     this.UpdateScore(0);
-    //     this.HideScore();
-    // }
+    showRessult(){       
+        console.log('alo');
+         
+        if(isNaN(parseInt(sys.localStorage.getItem('score')))){
+            sys.localStorage.setItem('score', this.currentScore.toString());
+            console.log("Yo it NaN");
+            
+        }else{
+            console.log('OPPPPPP');
+            this.highScore = Math.max(parseInt(sys.localStorage.getItem('score')), this.currentScore);
+            sys.localStorage.setItem('score', this.highScore.toString());
+        }
 
-    showRessult(){
-        this.highScore = Math.max(parseInt(sys.localStorage.getItem('score')), this.currentScore);
-        sys.localStorage.setItem('score', this.highScore.toString());
+        console.log(this.highScore+" high score");
 
-        console.log('high score>>>:>: ' + sys.localStorage.getItem('score'));
-
-        let temp = sys.localStorage.getItem('score');
-        //console.log('temp: ' + temp);
         
-        this.highScoreLabel.string = "High Score: " + temp;
+        this.highScoreLabel.string = "High Score: " + parseInt(sys.localStorage.getItem('score'));
         this.highScoreLabel.node.active = true;
         this.restartButton.node.active = true;
     }
-    // HideScore(){
-    //     this.highScoreLabel.node.active = false;
-    //     this.tryAgainLable.node.active = false;
-    // }
 
 }
 
